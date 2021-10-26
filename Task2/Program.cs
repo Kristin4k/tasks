f12778d939cea1627d55ef3957a3a62fc00514c6
Kristin4k <kristina.russkina03@mail.ru>
2021-10-26 23:33:20 +0500

    Выполнила задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите гипотенузу");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int x = 4 * a * 2;
+            int k = a * (a + b);
+            Console.WriteLine("Площадь вписанного квадрата в окружность " + x);
+            Console.WriteLine("Площадь вписанного правильного треугольнака в окружность " + k);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
