1a734074042d0c52e4e732aa4963d0e3b46c954a
Kristin4k <kristina.russkina03@mail.ru>
2021-12-26 19:43:32 +0500

    Выполнила задание 5.1

---
+++ Task5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите слово чемодан, чтобы получить производные слова от него");
+            string a = Console.ReadLine();
+            string word = "чемодан";
+            Console.WriteLine(a.Replace("чемодан", "демон"));
+            Console.ReadKey();
+            Console.WriteLine(a.Replace("чемодан", "меч"));
+            Console.ReadKey();
+        }
+    }
+}
