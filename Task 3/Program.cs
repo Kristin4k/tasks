4a1c9d8b996f9b36f54e958f3084e6d2d6aa1ba2
Kristin4k <kristina.russkina03@mail.ru>
2021-12-26 20:27:20 +0500

    Переделала задание 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычислите функцию y = f(x)");
+            Console.WriteLine("Введите значение переменной x");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f(x) = {Function(x):F3}");
+
+            Console.ReadKey();
+        }
+        static double Function(double x)
+        {
+            return Math.Sqrt(Math.Sin(x) * Math.Sin(2 * x) * Math.Sin(3 * x) * Math.Sin(4 * x));
+        }
+    }
+}
