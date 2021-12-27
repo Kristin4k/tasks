694487d0080527c13796c3d7857a11fcdc630e04
Kristin4k <kristina.russkina03@mail.ru>
2021-12-27 16:45:16 +0500

    Неокончательное примерное решениие №9

---
+++ Exam 9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowelletters = new char[20] { 'A', 'O', 'Э', 'Е', 'И', 'Ы', 'У', 'Ё', 'Ю', 'Я', 'а', 'о', 'э', 'е', 'и', 'ы', 'у', 'ё', 'ю', 'я' };
+
+            Console.WriteLine("Введите любое русское слово, словосочетание или предложение:");
+            var input = Console.ReadLine();
+            int count = 0;
+ 
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int e = 0; e < vowelletters.Length; e++)
+                {
+                    if (vowelletters[e] == input[i]) { count++; }
+                }
+            }
+            Console.WriteLine($"Количество гласных букв в этом слове, словосочетании или предложении: {count}");
+            Console.ReadLine();
+
+        }
+    }
+}
