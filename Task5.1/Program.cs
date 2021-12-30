7b3cb36d087d12caa18ae1a91d06a5357aeb07a4
Kristin4k <kristina.russkina03@mail.ru>
2021-12-30 06:05:57 +0500

    Переделала задание 5.1

--- Task5.1/Program.cs
+++ Task5.1/Program.cs
@@ -10,12 +10,19 @@ namespace Task5._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("введите слово чемодан, чтобы получить производные слова от него");
-            string a = Console.ReadLine();
-            string word = "чемодан";
-            Console.WriteLine(a.Replace("чемодан", "демон"));
-            Console.ReadKey();
-            Console.WriteLine(a.Replace("чемодан", "меч"));
+            Console.WriteLine("получить производные слова от чемодан");
+            Console.ReadLine();
+            string s1 = "ч";
+            string s2 = "е";
+            string s3 = "м";
+            string s4 = "о";
+            string s5 = "д";
+            string s6 = "а";
+            string s7 = "н";
+            string word1 = s5 + s2 + s3 + s4 + s7;
+            string word2 = s3 + s2 + s1;
+            Console.WriteLine(word1);
+            Console.WriteLine(word2);
             Console.ReadKey();
         }
     }
