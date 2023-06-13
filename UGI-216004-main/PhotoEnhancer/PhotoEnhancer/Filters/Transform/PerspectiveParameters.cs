2687fe4cf9ca265e420bfe2c7149e4861125c585
Kristin4k <kristina.russkina03@mail.ru>
2023-06-13 22:21:44 +0500

    Выполнила задание 6

---
+++ UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/Transform/PerspectiveParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class PerspectiveParameters : IParameters
+    {
+        [ParameterInfo(Name = "Процент сужения",
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 100,
+                    Increment = 5)]
+        public double NInPercents { get; set; }
+    }
+}
---
+++ UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/Transform/PerspectiveTransformer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class PerspectiveTransformer : ITransformer<PerspectiveParameters>
+    {
+        double N { get; set; }
+
+        public Size Size { get; private set; }
+        public Size ResultSize { get => Size; }
+
+        public void Initialize(Size size, PerspectiveParameters parameters)
+        {
+            N = (parameters.NInPercents) / 100;
+            Size = size;
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            point = new Point(point.X, point.Y);
+            var x = (int)(point.X);
+            var y = (int)(point.Y);
+
+            double midX = Size.Width / 2.0;
+            double factor = 1 - ((1 - N) * point.Y / Size.Height);
+
+            x = (int)(midX + (point.X - midX) / factor);
+
+            if (x < 0 || x >= Size.Width)
+                return null;
+
+            return new Point(x, y);
+        }
+    }
+}
